4c4c432dbca9e34d4cdcb4b7d81acea7bbbed43a
Tim <kimtimmy93@gmail.com>
2024-03-18 22:33:32 -0700

    add views/templates/login and signup templates

--- website/__init__.py
+++ website/__init__.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 def create_app():
   app = Flask(__name__)
+  app.config['SECRET_KEY'] = 'HAHAHA'
 
-  
+  from .views import views
+  from .auth import auth
+
+  app.register_blueprint(views, url_prfix='/')
+  app.register_blueprint(auth, url_prfix='/')
+
+  return app
+
+
